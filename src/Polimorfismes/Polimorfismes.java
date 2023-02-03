89255ddfddfc852fa8139d0c7f5907a3503b61d6
juankitavelita <jarevalo@nigul.cide.es>
2023-02-03 20:21:52 +0100

    Actualización

--- src/Polimorfismes/Polimorfismes.java
+++ src/Polimorfismes/Polimorfismes.java
@@ -1,26 +1,21 @@
 package Polimorfismes;
 public class Polimorfismes {
+    static Vehiculo[] vehiculos = new Vehiculo[4];
+    static Remolque[] remolques = new Remolque[2];
     public static void main(String[] args) throws DemasiadoRapidoException{
-        Camion c10 = new Camion ("PAPO 1234");
-        c10.Acelerar(80);
-        Remolque r10 = new Remolque(50);
-        c10.ponRemolque(r10);
-        c10.Acelerar(50);
-        System.out.println(c10);
-        System.out.println("------------------");
-        Vehiculo v1 = new Vehiculo ("VOX 1234");
-        v1.Acelerar(20);
-        System.out.println(v1);
-        Camion c1 = new Camion("SEX 4321");
-        c1.Acelerar(150);
-        System.out.println(c1);
-        Remolque r1 = new Remolque(70);
-        System.out.println(r1);
-        c1.ponRemolque(r1);
-        System.out.println(c1);
-        c1.quitaRemolque();
-        System.out.println(c1);
-        Camion c2 = new Camion("WOW 0000", r1);
-        System.out.println(c2);
+        vehiculos[0] = new Camion("DJG 4634");
+        vehiculos[1] = new Camion("LDS 9376");
+        vehiculos[2] = new Coche("NYH 9983");
+        vehiculos[3] = new Coche("BEG 2945");
+        Remolque r1 = new Remolque(5000);
+        Remolque r2 = new Remolque(5000);
+        int j = 0;
+        for(int i = 0; i > 3; i++) {
+            if (vehiculos[i] instanceof Camion) {
+
+            }
+            vehiculos[i].Acelerar(50);
+            System.out.println(vehiculos[i]);
+        }
     }
 }
