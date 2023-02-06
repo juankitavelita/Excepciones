dba920f66708161deb4d350e8be7bd49615663b0
juankitavelita <jarevalo@nigul.cide.es>
2023-02-06 17:23:31 +0100

    Actualización

--- src/Polimorfismes/Polimorfismes.java
+++ src/Polimorfismes/Polimorfismes.java
@@ -1,21 +1,29 @@
 package Polimorfismes;
+/**
+ * @author Juan Carlos Arévalo
+ * @para Exercici Polimorfismes
+ * @version 1.0*/
 public class Polimorfismes {
+    /**
+     * Creo el array de vehículos*/
     static Vehiculo[] vehiculos = new Vehiculo[4];
-    static Remolque[] remolques = new Remolque[2];
     public static void main(String[] args) throws DemasiadoRapidoException{
+        /**
+         * Creamos los objetos en el array*/
         vehiculos[0] = new Camion("DJG 4634");
         vehiculos[1] = new Camion("LDS 9376");
         vehiculos[2] = new Coche("NYH 9983");
         vehiculos[3] = new Coche("BEG 2945");
         Remolque r1 = new Remolque(5000);
-        Remolque r2 = new Remolque(5000);
-        int j = 0;
-        for(int i = 0; i > 3; i++) {
+        /**
+         * Un buqule for que primero comprueba si el objeto es un camión, si lo es le coloca un remolque llamando al método ponRemolque, y después o si no es camión, lo acelera y lo imprime por pantalla*/
+        for(int i = 0; i < 4; i++) {
             if (vehiculos[i] instanceof Camion) {
-
+                ((Camion) vehiculos[i]).ponRemolque(r1);
             }
             vehiculos[i].Acelerar(50);
             System.out.println(vehiculos[i]);
+            System.out.println("-------------------------------");
         }
     }
 }
